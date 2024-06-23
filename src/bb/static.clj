cf6f9a047580f59d8e0e98e18bd182511a15a07d
Charles Comstock <dgtized@gmail.com>
2024-06-23 18:45:02 -0500

    extract page helper in bb.static

--- src/bb/static.clj
+++ src/bb/static.clj
@@ -7,20 +7,25 @@
    [bb.pages.publications :as publications]
    [bb.revision :as rev]))
 
+(defn page [{:keys [file title body]}]
+  (println "Generating" (str file) "from"
+           (subs (rev/git-revision) 0 8)
+           "at"
+           (rev/timestamp-iso8601))
+  (spit file
+        (layout/layout {:title title}
+                       (layout/main body))))
+
 (defn build []
   (let [out-dir (fs/file "static")]
     (fs/delete-tree out-dir)
     (fs/create-dir out-dir)
-    (println "Generating index.html from"
-             (subs (rev/git-revision) 0 8)
-             "at"
-             (rev/timestamp-iso8601))
-    (spit (fs/file out-dir "index.html")
-          (layout/layout {:title "dgtized"}
-                         (layout/main [:article
-                                       (portfolio/section)
-                                       (exhibitions/section)
-                                       (publications/section)])))
+    (page {:file (fs/file out-dir "index.html")
+           :title "dgtized"
+           :body [:article
+                  (portfolio/section)
+                  (exhibitions/section)
+                  (publications/section)]})
     (fs/copy (fs/file "resources/public/style.css") (fs/file out-dir "style.css")
              {:replace-existing true})))
 
